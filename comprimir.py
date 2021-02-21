ff7e98926ae7bc17ecea5b6db2f34a14bd40c16f
sebastianmancera12@gmail.com <jsmancerag@correo.udistrital.edu.co>
2021-02-21 14:38:48 -0500

    Initial commit

---
+++ comprimir.py
@@ -0,0 +1,167 @@
+import numpy as np
+import math
+import os
+from PIL import Image as im
+salida = []
+from PIL import Image
+
+
+def leerMensaje():
+    archivo = open("mensaje.txt", "r")
+    mensaje = archivo.read()
+    archivo.close()
+    return mensaje
+
+
+def leerCaracteres(mensaje):
+    for i in mensaje:
+        print(i)
+
+
+def cantidadCaracteres(mensaje):
+    contador_letras = 0
+    for i in mensaje:
+        contador_letras += 1
+    return contador_letras
+
+
+def crearDiccionario(mensaje):
+    diccionario = {}
+    numeroletra = -1
+    for i in mensaje:
+        if i not in diccionario:
+            numeroletra += 1
+            diccionario[i] = numeroletra
+    return diccionario
+
+
+def agregarAlDiccionario(diccionario, clave, valor):
+    diccionario[clave] = valor
+    return diccionario
+
+
+def imprimir(diccionario,codigo):
+    impresion = diccionario[codigo]
+    return impresion
+
+
+
+def algoritmolzw(contador_letras,diccionario,mensaje):
+    """
+    Se crea una matriz de tres columnas y n filas para almacenar las columnas w,k,wk
+    Primera columna w
+    Segunda columna k
+    Tercera columna wk
+    """
+
+    matriz = np.zeros(shape=(contador_letras + 1, 3), dtype="U100")
+    fin_archivo = False
+    posicion_codigo = 0
+    posicion_fila = 0
+
+    # se asignan los valores de la columna  de la matriz segun el nombre
+    w = 0
+    k = 1
+    wk = 2
+
+    # incio algoritmo lzw
+    # w = null
+    matriz[posicion_fila, w] = "vacio"
+
+    # mientras no sea fin de archivo
+    while not fin_archivo:
+        # leer K
+        matriz[posicion_fila,k] = mensaje[posicion_codigo]
+        # wk = w + k
+        if matriz[posicion_fila, w] == "vacio":
+            matriz[posicion_fila, wk] = matriz[posicion_fila,k]
+        else:
+            matriz[posicion_fila, wk] = matriz[posicion_fila, w ] + matriz[posicion_fila,k]
+        posicion_codigo += 1
+        posicion_fila += 1
+        # si wk esta en el diccionario
+        if matriz[posicion_fila - 1,wk] in diccionario.keys():
+            # w = wk
+            matriz[posicion_fila, w] = matriz[posicion_fila -1, wk]
+        else:
+            # imprimir el código  w
+            impresion = imprimir(diccionario, matriz[posicion_fila -1, w])
+            salida.append(impresion)
+            # agregar wk al diccionario
+            numero_final = diccionario.values()
+            # se halla el ultimo valor asignado al diccionario
+            numero_final = max([int(i) for i in numero_final])
+            agregarAlDiccionario(diccionario,matriz[posicion_fila -1 ,wk],numero_final + 1 )
+            # w=K
+            matriz[posicion_fila,w] = matriz[posicion_fila-1,k]
+        # fin de archivo
+        if posicion_codigo == contador_letras:
+            matriz[posicion_fila, k] = "<EOF>"
+            fin_archivo = True
+    # imprimir el codigo de w
+    impresion = imprimir(diccionario, matriz[posicion_fila, w])
+    salida.append(impresion)
+    return matriz
+
+
+
+def cantidadBitsMensaje(contador_letras):
+    bits_mensaje = contador_letras * 8
+    return bits_mensaje
+
+
+def cantidadBitsCodigos(diccionario, salida):
+    bits_codigo = list(diccionario.values())
+    bits_codigo = max(bits_codigo) + 1
+    bits_codigo = math.ceil(math.log(bits_codigo, 2))
+    total_bits_codigo = bits_codigo * len(salida)
+    return total_bits_codigo
+
+
+def relacionCompresion(bits_mensaje, bits_salida):
+    rc = round(bits_mensaje / bits_salida, 3)
+    print(f"En LZW un bit en el codigo resultante representa {rc} bits de la version sin comprimir")
+
+
+def evaluacionCompresion(cantidad_letras, diccionario, salida):
+    bits_mensaje = cantidadBitsMensaje(cantidad_letras)
+    bits_salida = cantidadBitsCodigos(diccionario, salida)
+    relacionCompresion(bits_mensaje, bits_salida)
+
+
+def guardarcompresion(diccionario, salida):
+    pos_mensaje = 0
+    pos_diccionario = 0
+    with open("mensaje_comprimido.txt", "w") as mensaje_archivo:
+        for simbolo, valor in diccionario.items():
+            pos_diccionario += 1
+            mensaje_archivo.write(f"{simbolo}:{valor}")
+            if pos_diccionario != len(diccionario.keys()):
+                mensaje_archivo.write("|")
+        mensaje_archivo.write("\n")
+        for mensaje in salida:
+            pos_mensaje +=1
+            if pos_mensaje == len(salida):
+                mensaje_archivo.write(f"{mensaje}")
+            else:
+                mensaje_archivo.write(f"{mensaje},")
+    mensaje_archivo.close()
+
+
+def main():
+    mensaje = leerMensaje()
+    print("Texto a comprimir:", mensaje, sep="\n")
+    cantidad_letras = cantidadCaracteres(mensaje)
+    diccionario = crearDiccionario(mensaje)
+    print("Diccionario original")
+    print(diccionario)
+    diccionario_original = diccionario.copy()
+    matriz = algoritmolzw(cantidad_letras,diccionario,mensaje)
+    print(matriz)
+    print("Diccionario construido")
+    print(diccionario)
+    print(salida)
+    evaluacionCompresion(cantidad_letras, diccionario, salida)
+    guardarcompresion(diccionario_original, salida)
+
+main()
---
+++ descomprimir.py
@@ -0,0 +1,142 @@
+import numpy as np
+
+def leer_archivo_comprimido(archivo):
+    archivo_comprimido = open(archivo, "r")
+    lines = []
+    for line in archivo_comprimido:
+        lines.append(line)
+    archivo_comprimido.close()
+    return lines
+
+
+def leer_codigo(lineas):
+    mensaje = lineas[1].split(",")
+    return mensaje
+
+def leer_diccionario(lineas):
+    dicc = lineas[0].split("|")
+    print(dicc)
+    diccionario_claves = []
+    # se leen las claves del diccionario
+    for lista in dicc:
+        for caracter in lista:
+            if caracter != ":":
+                diccionario_claves.append(caracter)
+            else:
+                break
+    # se leen los valores del diccionario
+    diccionario_valores = []
+    for lista in dicc:
+        cadena = ""
+        leer = False
+        for caracter in lista:
+            if caracter == ":":
+                leer = True
+                continue
+            if leer:
+                cadena += caracter
+        diccionario_valores.append(cadena.rstrip("\n"))
+
+    #diccionario_valores.remove("\n")
+    diccionario = dict(zip(diccionario_claves, diccionario_valores))
+    return diccionario
+
+
+
+def contador_letras(codigo):
+    return len(codigo) - 1
+
+
+def traducir(codigo_viejo,diccionario):
+    clave = list(diccionario.keys())[list(diccionario.values()).index(codigo_viejo)]
+    return clave
+
+
+def algoritmo_descompresion(codigo,diccionario, contar_letras,):
+    # se crea la matriz donde se almacenará el código
+    matriz = np.zeros(shape=(contar_letras + 1, 5), dtype="U100")
+    posicion_fila = 0
+    posicion_codigo = 0
+    fin_archivo = False
+
+    # se asignan los valores de la columna  de la matriz segun el nombre
+    cod_viejo = 0
+    cod_nuevo = 1
+    cadena = 2
+    caracter = 3
+    salida = 4
+
+    # incio algoritmo descompresion
+
+    # leer codigo viejo
+    matriz[posicion_fila, cod_viejo] = codigo[posicion_codigo]
+    # caracter = Traducir(cód_viejo)
+    matriz[posicion_fila, caracter] = traducir(matriz[posicion_fila, cod_viejo],diccionario)
+    # imprimir caracter en salida
+    matriz[posicion_fila, salida] = matriz[posicion_fila, caracter]
+    # mientras no sea fin de archivo
+    while not fin_archivo:
+        # se itera al siguiente codigo
+        posicion_codigo += 1
+        # leer codigo nuevo
+        matriz[posicion_fila, cod_nuevo] = codigo[posicion_codigo]
+        # si codigo nuevo no esta en el diccionario
+        if codigo[posicion_codigo] not in diccionario.values():
+            # cadena=Traducir(cód_viejo)
+            matriz[posicion_fila, cadena] = traducir(matriz[posicion_fila, cod_viejo],diccionario)
+            # cadena= cadena + caracter
+            matriz[posicion_fila, cadena + 1] = matriz[posicion_fila, cadena] + matriz[posicion_fila, caracter]
+        else:  # cadena = traducir codigo nuevo
+            matriz[posicion_fila, cadena] = traducir(matriz[posicion_fila, cod_nuevo],diccionario)
+        posicion_fila = posicion_fila + 1
+        # imprimir cadena en salida
+        matriz[posicion_fila, salida] = matriz[posicion_fila - 1, cadena]
+        # caracter = primer caracter de cadena
+        matriz[posicion_fila, caracter] = str(matriz[posicion_fila - 1, cadena])[0]
+        # Agregar Traducir(cód_viejo)+carácter al diccionario
+        numero_final = diccionario.values()
+        # se halla el ultimo valor asignado al diccionario
+        numero_final = max([int(i) for i in numero_final])
+        diccionario[traducir(matriz[posicion_fila - 1, cod_viejo],diccionario) + matriz[posicion_fila, caracter]] = str(
+            numero_final + 1)
+        # cód_viejo=cód_nuevo
+        matriz[posicion_fila, cod_viejo] = matriz[posicion_fila - 1, cod_nuevo]
+        # comprobar fin de archivo en cada iteración
+        if posicion_codigo == contar_letras:
+            matriz[posicion_fila, cod_nuevo] = "<EOF>"
+            fin_archivo = True
+    return matriz
+
+
+def obtener_mensaje(matriz):
+    salida = 4  # numero de la columna a obtener de la matriz
+    columna = []
+    for fila in matriz:
+        columna.append(fila[salida])
+    return "".join(columna)
+
+def guardar_descompresion(mensaje):
+    with open("mensaje_descomprimido.txt", "w") as mensaje_archivo:
+        mensaje_archivo.write(mensaje)
+
+def main():
+    # leer archivo
+    archivo = leer_archivo_comprimido("mensaje_comprimido.txt")
+    # leer codigo
+    codigo = leer_codigo(archivo)
+    # leer diccionario
+    diccionario = leer_diccionario(archivo)
+    # se obtiene la cantidad de caracteres del mensaje comprimido para conocer el tamaño de la matriz
+    cant_letras = contador_letras(codigo)
+    # se crea la matriz del tamaño de codigos existentes en el mensaje y se descomprime
+    matriz = algoritmo_descompresion(codigo,diccionario, cant_letras)
+    print(matriz)
+    print(diccionario)
+    # se obtiene en mensaje de la columna 4 (salida) de la matriz
+    mensaje = obtener_mensaje(matriz)
+    print(f"El mensaje obtenido es: {mensaje}")
+    # se guarda mensaje en archivo
+    guardar_descompresion(mensaje)
+
+
+main()
